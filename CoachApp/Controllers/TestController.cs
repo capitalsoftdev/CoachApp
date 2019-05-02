660bf651d5c99605ef4bb57f3f5740baeac213c0
Harut <Harut@DESKTOP-RHKRDAV>
2019-05-02 19:19:51 +0400

    Add project files.

---
+++ CoachApp/Controllers/TestController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoachApp.Data;
+using CoachApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoachApp.Controllers
+{
+    public class TestController : Controller
+    {
+        private ApplicationDbContext _context;
+        public TestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tests = _context.Tests.Include(t => t.Participants).OrderByDescending(t => t.Date).ToList();
+            return View(tests);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Coach")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Coach")]
+        public async Task<IActionResult> Create(
+            [Bind("Type,Date")] Test test)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tests.Add(test);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Details(string id)
+        {
+            var test = _context.Tests
+                .Include(t => t.Participants)
+                    .ThenInclude(p => p.User)
+                .FirstOrDefault(t=>t.Id == id);
+            return View(test);
+        }
+
+        [Authorize(Roles = "Coach")]
+        public async Task<IActionResult> Delete(string id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var test = await _context.Tests
+                .FirstOrDefaultAsync(t => t.Id == id);
+            if (test == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
+            }
+
+            return View(test);
+        }
+
+        [Authorize(Roles = "Coach")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var test = await _context.Tests
+                .SingleOrDefaultAsync(t => t.Id == id);
+            if (test == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+                _context.Tests.Remove(test);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex)
+            {
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+        }
+    }
+}
---
+++ CoachApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CoachApp.Data;
+using CoachApp.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CoachApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    DbInitializer.Initialize(
+                        services.GetRequiredService<UserManager<ApplicationUser>>(),
+                        services.GetRequiredService<RoleManager<IdentityRole>>());
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
